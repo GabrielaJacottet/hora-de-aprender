0ce2fdf3ebd2a4483ace25401829596ce5e8cb3e
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2019-06-09 23:28:52 -0300

    Criação do menu

---
+++ Hora de Aprender/Assets/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void LoadSceneGame(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
