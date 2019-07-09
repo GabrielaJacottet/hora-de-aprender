ebe2606fb03e4557131dd527d71a1bdde9007706
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2019-07-08 22:45:54 -0300

    Começando a Dica

---
+++ Hora de Aprender/Assets/Scripts/Game5/MostrarDica.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MostrarDica : MonoBehaviour
+{ 
+
+    public string[] Characteres;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
