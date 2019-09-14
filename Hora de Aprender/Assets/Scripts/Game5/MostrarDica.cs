fccf01d607c4e24c8c80c14726e247db42e67a89
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2019-09-13 22:55:20 -0300

    colocando mais de um personagem para encontrar

--- Hora de Aprender/Assets/Scripts/Game5/MostrarDica.cs
+++ Hora de Aprender/Assets/Scripts/Game5/MostrarDica.cs
@@ -26,22 +26,16 @@ public class MostrarDica : MonoBehaviour
     private Vector3 PositionCircle1;
     private Vector3 PositionCircle2;
     private Vector3 PositionCircle3;
+    private bool Encontrou = false;
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        PositionCircle1 = new Vector3(-100f, -127f, 0);
-        PositionCircle2 = new Vector3(-30, -127f, 0);
-        PositionCircle3 = new Vector3(40f, -127f, 0);
+        PosicoesCirculos();
 
-        //adicionando os personagens na lista
-        ListaCharacters.Add("TipReindeer");
-        ListaCharacters.Add("TipPenguin");
-        /*ListaCharacters.Add("TipPolarBear");
-        ListaCharacters.Add("TipSnowman");
-        ListaCharacters.Add("TipSiberianHusky");
-        ListaCharacters.Add("TipSantaClaus");*/
+        AdicionarPersonagensLista();
 
         //Enquanto houver personagem na lista
         if (ListaCharacters.Count > 0)
@@ -53,30 +47,79 @@ public class MostrarDica : MonoBehaviour
         //Removendo o Personagem sorteado da lista, para que ele não seja desabilitado no próximo comando
         ListaCharacters.RemoveAt(SorteioDica);
 
-        //para cada personagem que não foi sorteado
-        //foreach (string i in ListaCharacters)
-        //    GameObject.Find(i).SetActive(false); // desative esse personagem
 
         //sorteando a posição que o personagem vai aparecer
         SorteioPosicao = Random.Range(0, 5);
 
-        //colocando as posições nas variáveis
-        if (SorteioPosicao == 0)
+        PosicionarPersonagemCena(SorteioPosicao);
+
+        HabilitandoPersonagem(FindCharacters);
+
+
+        // GameObject.Find(FindCharacters).this.GetComponent<Ima>
+        /* foreach (GameObject Characters in goCharacters) 
+         {
+             Characters.SetActive(true);
+       */
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        if(Encontrou)
+        {
+            if (ListaCharacters.Count > 0)
+                SorteioDica = Random.Range(0, ListaCharacters.Count - 1); //Sorteie um personagem
+            //Pegando o nome do personagem sorteado
+            FindCharacters = ListaCharacters[SorteioDica];
+
+            //Removendo o Personagem sorteado da lista, para que ele não seja desabilitado no próximo comando
+            ListaCharacters.RemoveAt(SorteioDica);
+            SorteioPosicao = Random.Range(0, ListaCharacters.Count - 1);
+
+            PosicionarPersonagemCena(SorteioPosicao);
+
+            HabilitandoPersonagem(FindCharacters);
+            Encontrou = false;
+        }
+
+    }
+
+    private void PosicoesCirculos()
+    {
+        PositionCircle1 = new Vector3(-100f, -127f, 0);
+        PositionCircle2 = new Vector3(-30, -127f, 0);
+        PositionCircle3 = new Vector3(40f, -127f, 0);
+    }
+
+    private void AdicionarPersonagensLista()
+    {
+        ListaCharacters.Add("TipReindeer");
+        ListaCharacters.Add("TipPenguin");
+        ListaCharacters.Add("TipPolarBear");
+        ListaCharacters.Add("TipSnowman");
+        ListaCharacters.Add("TipSiberianHusky");
+        ListaCharacters.Add("TipSantaClaus");
+    }
+
+    private void PosicionarPersonagemCena(int SorteioPos)
+    {
+        if (SorteioPos == 0)
             Position = new Vector3(220f, -70f, 0);//trenó
-        else if (SorteioPosicao == 1)
+        else if (SorteioPos == 1)
             Position = new Vector3(255f, 70f, 0);//igreja
-        else if (SorteioPosicao == 2)
+        else if (SorteioPos == 2)
             Position = new Vector3(-135f, 20f, 0);//iglu
-        else if (SorteioPosicao == 3)
+        else if (SorteioPos == 3)
             Position = new Vector3(15f, 90f, 0);//atrás casa
-        else if (SorteioPosicao == 4)
+        else if (SorteioPos == 4)
             Position = new Vector3(-300f, 25f, 0);//pinheiro
-        else if (SorteioPosicao == 5)
+        else if (SorteioPos == 5)
             Position = new Vector3(-120f, 85f, 0);//frente casa
+    }
 
-
-
-        //Habilitando o personagem que deve ser encontrado
+    private void HabilitandoPersonagem(string FindCharacters)
+    {
         if (FindCharacters == "TipReindeer")
         {
             if (FindReindeer != null)
@@ -95,88 +138,103 @@ public class MostrarDica : MonoBehaviour
         }
         else if (FindCharacters == "TipPolarBear")
         {
-            FindPolarBear.enabled = true;
-            FindPolarBear.transform.localPosition = Position;
+            if (FindPolarBear != null)
+            {
+                FindPolarBear.enabled = true;
+                FindPolarBear.transform.localPosition = Position;
+            }
         }
         else if (FindCharacters == "TipSnowman")
         {
-            FindSnowman.enabled = true;
-            FindSnowman.transform.localPosition = Position;
+            if (FindSnowman != null)
+            {
+                FindSnowman.enabled = true;
+                FindSnowman.transform.localPosition = Position;
+            }
         }
         else if (FindCharacters == "TipSiberianHusky")
         {
-            FindSiberianHusky.enabled = true;
-            FindSiberianHusky.transform.localPosition = Position;
+            if (FindSiberianHusky != null)
+            {
+                FindSiberianHusky.enabled = true;
+                FindSiberianHusky.transform.localPosition = Position;
+            }
         }
         else if (FindCharacters == "TipSantaClaus")
         {
-            FindSantaClaus.enabled = true;
-            FindSantaClaus.transform.localPosition = Position;
+            if (FindSantaClaus != null)
+            {
+                FindSantaClaus.enabled = true;
+                FindSantaClaus.transform.localPosition = Position;
+            }
         }
-
-
-        // GameObject.Find(FindCharacters).this.GetComponent<Ima>
-        /* foreach (GameObject Characters in goCharacters) 
-         {
-             Characters.SetActive(true);
-         }*/
-
-
-
 
     }
 
+    private void HabilitarCirculo2()
+    {
+        Circle1 = false;
+        Circle2 = true;
+    }
+
+    private void HabilitarCirculo3()
+    {
+        Circle1 = false;
+        Circle2 = false;
+        Circle3 = true;
+    }
+
+    private void ColocarPersonagemCirculo(string Personagem, Image FindCharacter)
+    {
+        if (Circle1)
+        {
+            FindCharacter.transform.localPosition = PositionCircle1;
+            HabilitarCirculo2();
+            Encontrou = true;
+        }
+        else if (Circle2)
+        {
+            FindCharacter.transform.localPosition = PositionCircle2;
+            HabilitarCirculo3();
+            Encontrou = true;
+        }
+        else if (Circle3)
+        {
+            FindCharacter.transform.localPosition = PositionCircle3;
+        }
+
+    }
+    
     public void Achou(string Personagem)
     {
         if(Personagem == "FindReindeer")
         {
-            if (Circle1)
-            {
-                FindReindeer.transform.localPosition = PositionCircle1;
-                Circle1 = false;
-                Circle2 = true;
-            }else if (Circle2)
-            {
-                FindReindeer.transform.localPosition = PositionCircle2;
-                Circle1 = false;
-                Circle2 = false;
-                Circle3 = true;
-            }
-            else if (Circle3)
-            {
-                FindReindeer.transform.localPosition = PositionCircle3;
-                //colocar animação
-            }
-        }else if (Personagem == "FindPenguin")
+            ColocarPersonagemCirculo(Personagem, FindReindeer);
+        }
+        else if (Personagem == "FindPenguin")
         {
-            if (Circle1)
-            {
-                FindPenguin.transform.localPosition = PositionCircle1;
-                Circle1 = false;
-                Circle2 = true;
-            }
-            else if (Circle2)
-            {
-                FindPenguin.transform.localPosition = PositionCircle2;
-                Circle1 = false;
-                Circle2 = false;
-                Circle3 = true;
-            }
-            else if (Circle3)
-            {
-                FindPenguin.transform.localPosition = PositionCircle3;
-                //colocar animação
-            }
+            ColocarPersonagemCirculo(Personagem, FindPenguin);
+        }
+        else if (Personagem == "FindPolarBear")
+        {
+            ColocarPersonagemCirculo(Personagem, FindPolarBear);
+        }
+        else if (Personagem == "FindSnowman")
+        {
+            ColocarPersonagemCirculo(Personagem, FindSnowman);
+        }
+        else if (Personagem == "FindSiberianHusky")
+        {
+            ColocarPersonagemCirculo(Personagem, FindSiberianHusky);
+        }
+        else if (Personagem == "FindSantaClaus")
+        {
+            ColocarPersonagemCirculo(Personagem, FindSantaClaus);
         }
 
-        
+
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-    }
+    
 }
