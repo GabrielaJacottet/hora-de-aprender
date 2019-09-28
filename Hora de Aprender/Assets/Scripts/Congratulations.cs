b85149fa5c93d5115469ca0dff52f96235803e91
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2019-09-28 17:00:45 -0300

    Criando Spawn dos Balões

--- Hora de Aprender/Assets/Scripts/Congratulations.cs
+++ Hora de Aprender/Assets/Scripts/Congratulations.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Congratulations : MonoBehaviour
 {
-    public float Velocidade = 500.0f;
+    //velocidade do balão
+    public float Velocidade = 100.0f;
     private Rigidbody2D Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = this.GetComponent<Rigidbody2D>();
+        //forçando a velocidade do balão
         Rigidbody.velocity = new Vector2(0, Velocidade);
 
-        GetComponent<Animator>().SetBool("PopBalloon", true);
+        PocouBalao("");
+        //GetComponent<Animator>().SetBool("PopBalloon", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //se o balão passar da tela, destruo ele
+        if(transform.position.y >= 480)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void PocouBalao(string nome)
+    {
+        GetComponent<Animator>().SetBool("PopBalloon", true);
+       // Destroy(this.gameObject);
     }
 }
---
+++ Hora de Aprender/Assets/Scripts/CriarBalao.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriarBalao : MonoBehaviour
+{
+    public GameObject BalaoPrefab;
+    public float TempoReaparecer = 1.0f;
+   // public string NomeBalao
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(OndaBalao());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AparecerBalao()
+    {
+        GameObject Balao = Instantiate(BalaoPrefab) as GameObject;
+        Balao.transform.position = new Vector3(0,0,0);
+        Balao.transform.parent = gameObject.transform;
+        //Balao.name = "teste";
+    }
+
+    IEnumerator OndaBalao()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(TempoReaparecer);
+            AparecerBalao();
+        }
+    }
+}
