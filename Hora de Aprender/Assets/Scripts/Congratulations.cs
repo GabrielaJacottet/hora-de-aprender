2013fcb552030731c099d790765931f897ed1432
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2019-09-27 23:03:27 -0300

    Criando animação dos balões e colocando o balão para subir

--- Hora de Aprender/Assets/Scripts/Congratulations.cs
+++ Hora de Aprender/Assets/Scripts/Congratulations.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Congratulations : MonoBehaviour
 {
+    public float Velocidade = 500.0f;
+    private Rigidbody2D Rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody = this.GetComponent<Rigidbody2D>();
+        Rigidbody.velocity = new Vector2(0, Velocidade);
+
+        GetComponent<Animator>().SetBool("PopBalloon", true);
     }
 
     // Update is called once per frame
